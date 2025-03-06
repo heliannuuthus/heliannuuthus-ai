0a67dafeeadeab8693fba9c1d6c4d73d445c1ee6
heliannutuhus <heliannuthus@gmail.com>
2025-03-06 22:53:13 +0800

    tauri

--- app/internal/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-from app.internal.exception import exception_handler, http_exception_handler
-
-
-logger = logging.getLogger(__name__)
-
-for logger_name in ["uvicorn", "uvicorn.access", "uvicorn.error", "fastapi"]:
-    logging.getLogger(logger_name).handlers = logger.handlers
-    
